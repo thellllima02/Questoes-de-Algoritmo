4ef631badf98a2a581c3955d750dfe93e83e13c8
Hércules Santos <116934166+thellllima02@users.noreply.github.com>
2022-11-14 11:57:25 -0300

    Update atividade_03.java

--- Recursão/atividade_03.java
+++ Recursão/atividade_03.java
@@ -3,24 +3,22 @@ import javax.swing.JOptionPane;
 public class atividade_03{
     
     public static int fatori(int m, int n ){
-        if(m>1){
-            return m + atividade_03.fatori(m, n-1);
+        if(m==0){
+            return 1;
         }
-        return 0;
+        else 
+        return x*fatori(x-1);
     }
 
     public static void main(String[] args) {
-        int m1,n1;
+        int m1;
         
         m1 = Integer.parseInt(JOptionPane.showInputDialog(null, " Entre com um numero "));
-        n1 = Integer.parseInt(JOptionPane.showInputDialog(null, " Entre com um numero "));
         
-        
-        
-        JOptionPane.showMessageDialog(null, " numero fatorial é "+atividade_03.fatori(m1,n1));
+        JOptionPane.showMessageDialog(null, " numero fatorial é = "+atividade_03.fatori(m1));
         
     }
     
 }
     
-
+//Como saber o numero fatorial.
