6411fbd30a98a53b964a60a46bf22a52bb2cd5a8
thellllima02 <thelllima02@hotmail.com>
2023-03-02 07:58:13 -0300

    Create teste.c
    Aprendendo C++

---
+++ Recursão/teste.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{ 
+ int no1, no2, sum ; 
+ cout << "\n Insira o primeiro número = " ; 
+ cin >> no1 ; 
+ cout << "\nInsira o segundo número = " ; 
+ cin >> no2 ; 
+ soma = no1 + no2; 
+ cout << "\n A soma de "<< no1 <<" e "<< no2 <<" = "<< sum '\n' ; 
+ return 0 ; 
+}
